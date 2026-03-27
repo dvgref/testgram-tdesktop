643afcf13c9ac34577d97e422a57c1671b173454
glebxdlolreal <nepexothnk@gmail.com>
2026-03-27 19:10:11 +0500

    Update mtproto_dc_options.cpp

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -28,8 +28,8 @@ struct BuiltInDc {
 	int port;
 };
 
-#define IpV4 "192.168.1.100"
-#define IpV6 "192.168.1.100"
+#define IpV4 "YOUR_SERVER_IP"
+#define IpV6 "YOUR_SERVER_IP"
 #define IpV4Port 20443
 #define IpV4Port2 20443
 #define IpV6Port 20443
